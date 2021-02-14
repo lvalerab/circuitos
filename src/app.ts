810425468c1446bf7103beff94991b03712d88a5
Luis Fernando Valera Bernal <lvalerab@gmail.com>
2021-02-14 14:07:46 +0100

    Subido la carpeta de test

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,14 @@ import {db} from './PERSISTENCIA/db';
 
 //Cargamos la configuracion
 require('dotenv').config();
-try {
+/*try {
     const cn=new db();
     cn.connect();
     cn.UpdateModel();
     cn.disconnect();
 } catch (error) {
 
-};
+};*/
 const SrvApi:RestServer=new RestServer();
 
 SrvApi.start(process.env.PORT?parseInt(process.env.PORT):8080);
